6a908af8f0ccf5a9476dcd3a2cb270ad8f663f60
Roberto Gaiser <gaiser@geekbunker.org>
2011-02-25 11:29:12 -0300

    cloudwatch fix in progress

--- boto/connection.py
+++ boto/connection.py
@@ -658,6 +658,7 @@ class AWSQueryConnection(AWSAuthConnection):
             boto.log.error('Null body %s' % body)
             raise self.ResponseError(response.status, response.reason, body)
         elif response.status == 200:
+            print body
             rs = ResultSet(markers)
             h = handler.XmlHandler(rs, parent)
             xml.sax.parseString(body, h)
